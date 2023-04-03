9498b2728ee60d88b738f70e5228da627d21add8
Mason Youngblood <masonyoungblood@gmail.com>
2023-04-03 11:23:40 +0200

    Updating disruption simulations

--- disruption_sims.R
+++ disruption_sims.R
@@ -8,15 +8,15 @@ source("functions.R")
 pkgs <- unique(getParseData(parse("functions.R"))$text[getParseData(parse("functions.R"))$token == "SYMBOL_PACKAGE"])
 
 #set parameters
-n <- 200
-t <- 1000
+n <- 10
+t <- 2000
 t_2 <- 200
 cost <- c(0.5, 0.5, 0.5, 0.5)
 moves <- c(2, 2, 2, 2)
 gamma <- c(0, 0.25, 0, 0.25)
 f <- c(0, 2.5, 0, 2.5)
 networked <- c(FALSE, FALSE, TRUE, TRUE)
-props <- seq(0.05, 0.95, by = 0.05)
+props <- seq(0.1, 0.9, by = 0.1)
 
 #set seed
 set.seed(123)
@@ -24,15 +24,15 @@ set.seed(123)
 #run simulations
 disruption_sims <- lapply(1:length(gamma), function(z){
   #first run base model and compute previous status quo
-  base <- model(pop_size = n, t = t, neg_cost = cost[z], n_moves = moves[z], gamma = gamma[z], f = f[z], networked = networked[z], last_output = TRUE)
-  status_quo <- which.max(as.numeric(table(factor(base$status_quo, levels = 1:moves[z]))))
+  base <- model(pop_size = n, t = t, neg_cost = cost[z], n_moves = moves[z], gamma = gamma[z], f = f[z], networked = networked[z])
+  status_quo <- which.max(as.numeric(table(factor(base[[t]]$status_quo, levels = 1:moves[z]))))
 
   #get frequency of previous status quo
-  base_freqs <- sapply(1:t, function(y){length(which(base$status_quo == status_quo))/n})
+  base_freqs <- sapply(1:t, function(y){length(which(base[[y]]$status_quo == status_quo))/n})
 
   #hard reset proportion of population with alternate status quo
   disrupt_freqs <- parallel::mclapply(1:length(props), function(x){
-    object <- base
+    object <- base[[t]]
     if(moves[z] == 2){
       new_status_quo <- c(1:moves[z])[-which.max(as.numeric(table(factor(object$status_quo, levels = 1:moves[z]))))]
     } else{
@@ -65,7 +65,7 @@ disruption_sims <- lapply(1:length(gamma), function(z){
     #run new simulations and store simplified output
     output <- model(pop_size = n, t = t_2, neg_cost = cost[z], n_moves = moves[z], supply_agents = object, gamma = gamma[z], f = f[z], networked = networked[z], simple_output = TRUE)
     return(do.call(rbind, lapply(1:length(output), function(y){output[[y]][[1]]})))
-  }, mc.cores = 19)
+  }, mc.cores = 9)
 
   #return all simplified output
   return(list(base_freqs = base_freqs, disrupt_freqs = disrupt_freqs))
